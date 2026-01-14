3c6ab04263acfc93d25c3becdab617db9743ef04
jaytrinidadpr <justinquelix@icloud.com>
2026-01-13 21:55:00 -0600

    part1 completed

--- index.js
+++ index.js
@@ -7,23 +7,25 @@ const state = {
 };
 
 function NumberBucket(title, number) {
-    const section = document.createElement("selection")
+    const section = document.createElement("section")
     section.className = "bucket";
     const h2 = document.createElement("h2");
+    h2.textContent = title;
     const list = document.createElement("div");
     list.className = "bucket-list";
 
-    list.textContent = SVGAnimatedNumberList.length
-    ? numbers.join(", ")
-    : "(empty)";
+    list.textContent = number.length ? number.join(", ") : "(empty)";
+
     section.append(h2, list);
     return section;
 }
 
 function Header() {
     const header = document.createElement("header");
+
     const h1 = document.createElement("h1")
-    history.textContent = "Odss and Evens";
+    h1.textContent = "Odds and Evens";
+
     const p = document.createElement("p");
     p.textContent =
     "Add numbers to the bank, then sort them into odds or evens";
@@ -38,15 +40,58 @@ function Controls() {
     wrapper.className = "controls";
 
     const form = document.createElement("form");
+
+    const input = document.createElement("input");
     input.type = "number";
     input.name = "number";
     input.placeholder = "Enter a number";
     input.required = true;
 
-    const addBtns = document.createElement("button");
+    const addBtn = document.createElement("button");
     addBtn.type = "submit";
     addBtn.textContent = "Add number";
 
     form.append(input, addBtn);
+//--------
+// Sort Buttons
+//--------
 
+const sort1Btn = document.createElement("button");
+sort1Btn.type = "button";
+sort1Btn.id = "sort-1";
+sort1Btn.textContent = "Sort 1";
+
+const sortAllBtn = document.createElement("button");
+sortAllBtn.type = "button";
+sortAllBtn.id = "sort-all";
+sortAllBtn.textContent = "Sort All";
+
+const btnRow = document.createElement("div");
+btnRow.className = "button-row";
+btnRow.append(sort1Btn, sortAllBtn);
+
+wrapper.append(form, btnRow);
+return wrapper;
 }
+
+function App() {
+
+    const container = document.createElement("div");
+    container.className = "container";
+
+    container.append(
+        Header(),
+        Controls(),
+        NumberBucket("Number Bank", state.bank),
+        NumberBucket("Odds", state.odds),
+        NumberBucket("Evens", state.evens)
+    );
+    return container;
+}
+
+function render() {
+    app.innerHTML = "";
+    app.appendChild(App());
+}
+
+render();
